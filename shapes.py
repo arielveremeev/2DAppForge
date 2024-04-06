3bbd16a5256c13b3db554f63ce61577c982204c4
arielveremeev <leva.veremeev@gmail.com>
2024-04-06 13:03:29 +0300

    fully working shapes file without draw function polygon and circle classes

--- shapes.py
+++ shapes.py
@@ -1,49 +1,37 @@
 class Shape():
-    def __init__(self):
-        self.shapes=[]
-        self.points-[]
-        self.name=""
-        self.vertexes=0
+    def __init__(self,line):
+        parameters=line.split(" ")
+        print(parameters)
+        self.name=parameters[0]
+        self.vertexes=parameters[1]
+        self.points=self.SetPoints(parameters[2])
         self.color="black"
 
-    def SetPoints(self,points):
-        count=0
-        for point in points:
-            x=0
-            y=0
-            if(count%2==0):
-                x=point
-            else:
-                y=point
-            if(x!=0 and y!=0):
-                temp=Point(x,y)
-                self.points.append(temp)
-        self.vertexes=count/2
+    def __str__(self) -> str:
+        return f"Name = {self.name} Points = {[str(obj) for obj in self.points]}"
+
+    def SetPoints(self,line):
+        coordinates=line.split(";")
+        points=[]
+        for i in range (0,len(coordinates),2):
+            x=coordinates[i]
+            y=coordinates[i+1]
+            points.append(Point(x,y))
+        return points
 
     def PrintPoints(self):
         for point in self.points:
             print(point)
-    
-    def AddShape(self,name):
 
-        if(name=="circle"):
-            shape=Circle()
-            self.name=name
-        
-        else:
-            shape=Polygon()
-            self.name=name
-
-    
-    def CreateShapes(self,lines):
-        count=0
-        for line in lines:
-
+            
 
 class Point():
     def __init__(self,x,y):
         self.x=float(x)
         self.y=float(y)
+
+    def __str__(self) -> str:
+        return f"[{str(self.x)},{str(self.y)}]"
 
 
 
@@ -51,16 +39,28 @@ class Polygon(Shape):
     pass
 
 class Circle(Shape):
-    def __init__(self):
-        self.center=[]
-        self.color="black"
-        self.radius=0
-    def SetRadius(self,radius):
-        self.radius=radius
-    def SetCenter(self,x,y):
-        center=Point(x,y)
-        self.center.append(center)
+    def __init__(self,line):
+        Shape.__init__(self,line)
+        self.radius=self.SetRadius(line)
 
+    def __str__(self) -> str:
+        return super(Circle,self).__str__() + "radius = " + str(self.radius)
+
+    def SetRadius(self,line):
+        para=line.split(" ")
+        return para[3]
+
+
+def CreateShapes(lines):
+    shapes=[]
+    for line in lines:
+        if("circle" in line):
+            shape=Circle(line)
+        else:
+            shape=Polygon(line)
+        print(shape)
+        shapes.append(shape)
+    return shapes
 
 def main():
 
@@ -69,16 +69,23 @@ def main():
     lines=[]
     while True:
         count+=1
-        lines.append(file.readline())
-
-        if not lines[count-1]:
+        line = file.readline()
+        line=line.rstrip()
+        
+        if not line:
             break
+        lines.append(line)
         print("Line{}: {}".format(count, lines[count-1].strip()))
     file.close()
 
-    for line in lines:
-        print(line)
-    
+    count=0
+
+    print("\n")
+
+    shapes=CreateShapes(lines)
+
+
+    print(shapes)
 
 if __name__ == "__main__":
     main()
