ccc96d176730dceea860dc5d3918859d66569983
Dmitry Veremeev <dveremeev@gmail.com>
2024-05-18 20:46:56 +0300

    fix open create session dialog after split sources

--- sessionlistframe.py
+++ sessionlistframe.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-
+from dialogs import CreateSessDialog
 class SessionListFrame(ttk.Frame):
     def __init__(self,parent,callbacks):
         ttk.Frame.__init__(self, parent)
